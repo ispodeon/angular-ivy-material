6bf24e2d2f8fbdbf6695a8d38a8dfaf116d61dab
ispodeon <ispodeon@yahoo.com>
2022-08-02 17:27:59 -0400

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+
+import { MaterialExampleModule } from '../material.module';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule, MaterialExampleModule],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
